dc96b138947676a966b17fcfedcec57994a32d2e
Harshil <harshilzaveri0307@gmail.com>
2020-02-06 04:21:51 +0530

    ui changes

--- app/src/main/java/com/example/tap_android/Login.java
+++ app/src/main/java/com/example/tap_android/Login.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.dd.processbutton.FlatButton;
+
 public class Login extends AppCompatActivity {
-    Button TapAdmin;
-    Button SchoolAdmin;
+    FlatButton TapAdmin;
+    FlatButton SchoolAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
